e1d1c488ae2a7343962260bf059bd4962734ebb5
Samiur-UTI <allpraisethelord@gmail.com>
2021-02-09 20:31:37 +0600

    server init

--- DB/index.js
+++ DB/index.js
@@ -5,6 +5,12 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.get("/", (req,res) => {
+    res.send("This is the root route")
+})
+app.post("/", (req,res) =>{
+    res.send("This is the post route")
+})
 const CONNECTION_URL = "";
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}
     ).then(() => app.listen(PORT, () => console.log(`Server running at ${PORT}`)))
