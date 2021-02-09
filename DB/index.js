9affa5fad5ab0048e8d65c264beaf2630ffd662a
Samiur-UTI <allpraisethelord@gmail.com>
2021-02-09 20:29:22 +0600

    server init

--- DB/index.js
+++ DB/index.js
@@ -1,5 +1,12 @@
-const express = require("express");
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+const PORT = process.env.PORT || 5000;
 const app = express();
-const PORT = 3000;
-
-app.listen(PORT, () => console.log(`server is running at port ${PORT}`));
+app.use(bodyParser.json({limit:"30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+const CONNECTION_URL = "";
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true}
+    ).then(() => app.listen(PORT, () => console.log(`Server running at ${PORT}`)))
+    .catch((error) => console.log(error.message));
+mongoose.set("useFindAndModify", false);
